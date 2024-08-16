dfe6b7e69b9d50664a97ac47014e5b49e3343301
Sakshi3025 <bhosalesakshi25.sb@gmail.com>
2024-08-16 16:19:58 +0530

    new commit

---
+++ js/auth.js
@@ -0,0 +1,30 @@
+// Login button Change
+document.addEventListener("DOMContentLoaded", function () {
+    const loginButtons = document.querySelectorAll(".loginButton");
+
+    const isLoggedIn = localStorage.getItem("isLoggedIn");
+
+    loginButtons.forEach((loginButton) => {
+        if (isLoggedIn === "true") {
+            loginButton.innerHTML = '<a href="#" class="logoutLink">Logout</a>';
+        } else {
+            loginButton.innerHTML = '<a href="login.html">Login</a>';
+        }
+    });
+
+    // Add event listeners for logout on each login/logout button
+    document.querySelectorAll(".logoutLink").forEach((logoutLink) => {
+        logoutLink.addEventListener("click", function (event) {
+            event.preventDefault();
+            localStorage.setItem("isLoggedIn", "false");
+            localStorage.removeItem("accessToken"); 
+            localStorage.removeItem("userRole"); 
+
+            // Reset button text to "Login" after logging out
+            loginButtons.forEach((loginButton) => {
+                loginButton.innerHTML = '<a href="login.html">LogIn</a>';
+            });
+            window.location.href = "index.html";
+        });
+    });
+});
